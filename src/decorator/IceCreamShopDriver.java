49bed8e13932e953086c8680e7eecd554ce0bdbe
Samuel Wu <twopizza9621536@gmail.com>
2025-09-02 11:12:59 -0400

    feat: ✨ Add driver files for the decorator pattern
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ src/decorator/IceCreamShopDriver.java
@@ -0,0 +1,62 @@
+package decorator;
+
+import java.util.Scanner;
+
+public class IceCreamShopDriver {
+  private Scanner reader;
+  private int numScoopsLeft;
+  private static final int MAX_SCOOPS = 6;
+  private static final String BLUEBERRY = "Blueberry";
+  private static final String VANILLA = "Vanilla";
+  private static final String STRAWBERRY = "Strawberry";
+
+  public IceCreamShopDriver() {
+    reader = new Scanner(System.in);
+    numScoopsLeft = MAX_SCOOPS;
+  }
+
+  public void order() {
+    IceCream iceCream = new Cone();
+
+    System.out.println("You can have a maximum of " + MAX_SCOOPS + " scoops.");
+    iceCream = addScoops(iceCream, BLUEBERRY);
+    iceCream = addScoops(iceCream, STRAWBERRY);
+    iceCream = addScoops(iceCream, VANILLA);
+
+    System.out.println("\nHere's your ice cream cone. \n");
+    System.out.println(iceCream);
+  }
+
+  public IceCream addScoops(IceCream iceCream, String flavor) {
+    int num = 0;
+
+    while (true) {
+      System.out.print("How many scoops of " + flavor + ": ");
+      num = Integer.parseInt(reader.nextLine());
+
+      if (num < 0 || num > numScoopsLeft) {
+        System.out.println("Sorry, you need to pick between 0 and " + numScoopsLeft);
+        continue;
+      }
+
+      numScoopsLeft -= num;
+      break;
+    }
+
+    if (num > 0) {
+      if (flavor.equals(BLUEBERRY)) {
+        return new BlueBerry(iceCream, num);
+      } else if (flavor.equals(STRAWBERRY)) {
+        return new Strawberry(iceCream, num);
+      } else if (flavor.equals(VANILLA)) {
+        return new Vanilla(iceCream, num);
+      }
+    }
+
+    return iceCream;
+  }
+
+  public static void main(String[] args) {
+    (new IceCreamShopDriver()).order();
+  }
+}
